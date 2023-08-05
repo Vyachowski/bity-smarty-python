18858c645a51d315f4bfa13a03ab222e82258980
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-05 15:37:19 +0300

    refactor: delete excess comments

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -37,6 +37,7 @@ def grocery_list_command(message):
     grocery_list = diet.get_grocery_list()
     bot.send_message(message.chat.id, grocery_list)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def button_callback(call):
     user_id = call.from_user.id
@@ -51,60 +52,3 @@ def button_callback(call):
 
 
 bot.infinity_polling()
-#
-# sys.path.append('../')
-#
-# diet = Diet()
-#
-# # async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-# #     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-#
-#
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     user_id = update.effective_user.id
-#     keyboard = [
-#         [InlineKeyboardButton("Get menu", callback_data='get_menu')]
-#         # [InlineKeyboardButton("Get grocery list",
-#         #                       callback_data='get_grocery_list')]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await context.bot.send_message(
-#         chat_id=user_id, text="Hi! Do you want to create a new menu?", reply_markup=reply_markup)
-#
-#
-# if __name__ == '__main__':
-#     bot = ApplicationBuilder().token(telegraf_token).build()
-#
-#     bot.add_handler(CommandHandler('start', start))
-#
-#
-#     def get_menu(update):
-#         diet.set_menu()
-#         menu_text = diet.get_menu()
-#         update.message.reply_text(menu_text)
-#
-#
-#     def get_grocery_list(update):
-#         diet.set_menu()
-#         grocery_list_text = diet.get_grocery_list()
-#         update.message.reply_text(grocery_list_text)
-#
-#
-#     def button_callback(update, context):
-#         query = update.callback_query
-#         user_id = query.from_user.id
-#         if query.data == 'get_menu':
-#             diet.set_menu()
-#             menu_text = diet.get_menu()
-#             context.bot.send_message(chat_id=user_id, text=menu_text)
-#         elif query.data == 'get_grocery_list':
-#             diet.set_menu()
-#             grocery_list_text = diet.get_grocery_list()
-#             context.bot.send_message(chat_id=user_id, text=grocery_list_text)
-#
-#
-#     bot.add_handler(CommandHandler('get_menu', get_menu))
-#     bot.add_handler(CommandHandler('get_grocery_list', get_grocery_list))
-#     bot.add_handler(CallbackQueryHandler(button_callback))
-#
-#     bot.run_polling()
